5af6424c00dda5d49625eaa3644c1457d5fc718d
“GiovanniDw” <giovannidwidjosewojo@hotmail.com>
2023-02-15 13:18:15 +0100

    api call now shows in frontend

--- spa/src/app.js
+++ spa/src/app.js
@@ -1,20 +1,47 @@
-const apiKey = import.meta.env.VITE_RIJKSMUSEUM_API;
-// const apiKey = 'S3GLzVAr';
+import { getMuseumData } from './getMuseumData';
 
+const apiKey = import.meta.env.VITE_RIJKSMUSEUM_API;
 const URL = `https://www.rijksmuseum.nl/api/en/collection?key=${apiKey}`;
 const rembrand = '&involvedMaker=Rembrandt+van+Rijn';
 
-const fullURL = URL + rembrand;
+const museumOptions = {
+  lang: 'en',
+  apiKey: apiKey,
+  color: '',
+  involvedMaker: 'Rembrand',
+  URL: URL,
+};
 
-fetchData();
-console.log(fullURL);
+const museumData = await getMuseumData(museumOptions);
+createMuseumItemWithTemplate(museumData);
 
-function fetchData() {
-  const data = fetch(fullURL)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
+
+function createMuseumItemWithTemplate(museumData) {
+  const museumItems = museumData.artObjects;
+console.log(museumItems);
+  const article = document.createElement('article');
+  article.classList.add('art');
+
+  const template = document.getElementById('museum-item');
+
+  museumItems.forEach((museumItem) => {
+    const articleItem = document.importNode(template.content, true);
+    articleItem.querySelector('.title').textContent = museumItem.longTitle;
+    articleItem.querySelector('.image').src = museumItem.webImage.url;
+    article.appendChild(articleItem);
+  });
+  template.replaceWith(article);
 }
 
-console.log(import.meta.env.VITE_RIJKSMUSEUM_API);
+// function createListWithTemplate(heroes: Hero[]) {
+//   const ul = document.createElement('ul');
+//   ul.classList.add('list', 'hero-list');
+//   const template = document.getElementById('museumItem') as HTMLTemplateElement;
+//   heroes.forEach((hero: Hero) => {
+//     const heroCard = document.importNode(template.content, true);
+//     heroCard.querySelector('.description').textContent = hero.description;
+//     heroCard.querySelector('.name').textContent = hero.name;
+//     ul.appendChild(heroCard);
+//   });
+//   heroPlaceholder.replaceWith(ul);
+// }
